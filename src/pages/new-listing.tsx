56d5f331b086e155d78b7c21be77c04f43a6036a
Paul Anthony <paulragsac@gmail.com>
2022-08-13 19:46:41 +0800

    trying out other form

--- src/pages/new-listing.tsx
+++ src/pages/new-listing.tsx
@@ -4,6 +4,7 @@ export default function NewListingForm() {
       <form className="w-full max-w-sm">
         <div className="flex items-center border-b border-teal-500 py-2">
           <input
+            required
             className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none
              focus:border-sky-500 focus:ring-1 focus:ring-sky-500
              invalid:border-pink-500 invalid:text-pink-600
@@ -12,19 +13,19 @@ export default function NewListingForm() {
             placeholder="Jane Doe"
             aria-label="Client Name/s"
           />
+          <button
+            className="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded"
+            type="button"
+          >
+            Sign Up
+          </button>
+          <button
+            className="flex-shrink-0 border-transparent border-4 text-teal-500 hover:text-teal-800 text-sm py-1 px-2 rounded"
+            type="button"
+          >
+            Cancel
+          </button>
         </div>
-        <button
-          className="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded"
-          type="button"
-        >
-          Sign Up
-        </button>
-        <button
-          className="flex-shrink-0 border-transparent border-4 text-teal-500 hover:text-teal-800 text-sm py-1 px-2 rounded"
-          type="button"
-        >
-          Cancel
-        </button>
       </form>
     </>
   );
