e5681510b2258bd5208425bf354fb9384fee0f32
Paul Anthony <paulragsac@gmail.com>
2022-08-13 19:07:16 +0800

    cleaner index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,34 +1,15 @@
-import { trpc } from "../utils/trpc";
+import Example from './example';
 import Header from './head';
-import ClientNameCreator from './clientNameCreator';
 import HomePath from "./link";
 
-
-
 export default function Home () {
-  const listing = trpc.useQuery(["form-client.get-all"]);
   return (
     <>
     <Header />
       <main className="container mx-auto flex flex-col items-center justify-center min-h-screen p-4">
         <HomePath />
         <div className="pt-6 text-2xl text-blue-500 flex justify-center items-center w-full">
-          {listing.data ? 
-          <div className="p-6 flex flex-col">
-            <div className="flex flex-col">
-            <div className="text-2xl font-bold"> Client Names<div/>
-            {listing.data.map((Client) => {
-              return (
-              <div key={Client.id} className="my-2">
-                {Client.clientNames}
-                </div>
-                );
-            })} 
-            </div>
-            </div>
-            <ClientNameCreator /> 
-            </div> 
-            : <p>Loading..</p>}
+          <Example />
         </div>
       </main>
     </>
