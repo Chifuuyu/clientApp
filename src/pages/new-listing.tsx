971df60bddf28c6c2de1ac0d0431dab286ba2d76
Paul Anthony <paulragsac@gmail.com>
2022-08-13 19:44:10 +0800

    trying other form

--- src/pages/new-listing.tsx
+++ src/pages/new-listing.tsx
@@ -1,23 +1,30 @@
 export default function NewListingForm() {
   return (
     <>
-      <form>
-        <label className="block">
-          <span className="block text-sm font-medium text-slate-700">
-            Username
-          </span>
+      <form className="w-full max-w-sm">
+        <div className="flex items-center border-b border-teal-500 py-2">
           <input
+            className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none
+             focus:border-sky-500 focus:ring-1 focus:ring-sky-500
+             invalid:border-pink-500 invalid:text-pink-600
+             focus:invalid:border-pink-500 focus:invalid:ring-pink-500"
             type="text"
-            value="tbone"
-            disabled
-            className="mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
-      focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
-      disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
-      invalid:border-pink-500 invalid:text-pink-600
-      focus:invalid:border-pink-500 focus:invalid:ring-pink-500
-    "
+            placeholder="Jane Doe"
+            aria-label="Client Name/s"
           />
-        </label>
+        </div>
+        <button
+          className="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded"
+          type="button"
+        >
+          Sign Up
+        </button>
+        <button
+          className="flex-shrink-0 border-transparent border-4 text-teal-500 hover:text-teal-800 text-sm py-1 px-2 rounded"
+          type="button"
+        >
+          Cancel
+        </button>
       </form>
     </>
   );
