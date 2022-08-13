a2149f2ac108ed642ecd4f2d211621593c97d9db
Paul Anthony <paulragsac@gmail.com>
2022-08-13 18:35:45 +0800

    seperated other functions to other components

---
+++ src/pages/clientNameCreator.tsx
@@ -0,0 +1,25 @@
+import { trpc } from "../utils/trpc";
+
+
+const ClientNameCreator: React.FC = () => {
+  const client = trpc.useContext();
+  const {mutate} = trpc.useMutation("form-client.create", {
+    onSuccess: () => {
+      client.invalidateQueries("form-client.get-all");
+    },
+  });
+
+return (
+  <input 
+    onKeyDown={(ev)=>{
+      if (ev.key === "Enter") {
+        console.log("Enter pressed!", ev.currentTarget.value);
+        mutate({clientNames: ev.currentTarget.value});
+        ev.currentTarget.value = "";
+        }
+    }}
+    ></input>
+);
+};
+
+export default ClientNameCreator;
---
+++ src/pages/link.tsx
@@ -0,0 +1,19 @@
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+
+export default function HomePath() {
+const router = useRouter();
+const currentRoute = router.pathname;
+return(
+<h1 className="text-5xl md:text-[5rem] leading-normal font-extrabold text-gray-700">
+<Link href="/">
+    <a className={currentRoute == "/" ? "active" : "non-active"}>
+    <span className="text-purple-300 active:text-gray-700">New Listing </span></a>
+    </Link>or 
+    <Link href="/">
+        <a className={currentRoute == "/" ? "active" : "non-active"}>
+            <span className="text-orange-300 active:text-gray-700">Under Contract</span></a>
+            </Link>
+</h1>
+);
+}
